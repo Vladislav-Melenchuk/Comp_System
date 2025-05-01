a33dfcf5ba030a11d547e5b977184fabc2b36355
Vladislav <10328026@stud.op.edu.ua>
2025-05-01 15:35:45 +0300

    add hw_4

---
+++ HW_4/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HW_4
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    
+      
+        private void InitializeComponent()
+        {
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxMask = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(30, 40);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(400, 27);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // textBoxMask
+            // 
+            this.textBoxMask.Location = new System.Drawing.Point(30, 100);
+            this.textBoxMask.Name = "textBoxMask";
+            this.textBoxMask.Size = new System.Drawing.Size(400, 27);
+            this.textBoxMask.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(30, 150);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(150, 40);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Почати пошук";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(210, 150);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(150, 40);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Скасувати пошук";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.ItemHeight = 20;
+            this.listBoxResults.Location = new System.Drawing.Point(30, 210);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(600, 200);
+            this.listBoxResults.TabIndex = 4;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(30, 430);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(58, 20);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Статус";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Шлях до директорії:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Маска пошуку:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 480);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxMask);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "Form1";
+            this.Text = "Пошук файлів";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+      
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxMask;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ HW_4/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_4
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource cancellationTokenSource;
+        private int foundFilesCount = 0;
+        private string logFilePath = "log.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            string directoryPath = textBoxPath.Text;
+            string searchPattern = textBoxMask.Text;
+
+            if (string.IsNullOrWhiteSpace(directoryPath) || string.IsNullOrWhiteSpace(searchPattern))
+            {
+                MessageBox.Show("Будь ласка, введіть шлях до папки та маску пошуку.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Directory.Exists(directoryPath))
+            {
+                MessageBox.Show("Вказана папка не існує.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+            if (!searchPattern.Contains("*"))
+            {
+                if (searchPattern.StartsWith("."))
+                    searchPattern = "*" + searchPattern;
+                else
+                    searchPattern = "*." + searchPattern;
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+            listBoxResults.Items.Clear();
+            labelStatus.Text = "Пошук виконується...";
+            foundFilesCount = 0;
+
+            try
+            {
+                using (StreamWriter logWriter = new StreamWriter(logFilePath, false))
+                {
+                    await Task.Run(() => SearchFilesParallel(directoryPath, searchPattern, cancellationTokenSource.Token, logWriter));
+                }
+
+                labelStatus.Text = $"Пошук завершено. Знайдено файлів: {foundFilesCount}";
+            }
+            catch (OperationCanceledException)
+            {
+                labelStatus.Text = "Пошук скасовано.";
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Text = "Помилка: " + ex.Message;
+            }
+        }
+
+        private void SearchFilesParallel(string directoryPath, string searchPattern, CancellationToken token, StreamWriter logWriter)
+        {
+            ConcurrentBag<string> allDirectories = new ConcurrentBag<string>();
+            allDirectories.Add(directoryPath);
+
+            try
+            {
+                foreach (var dir in Directory.EnumerateDirectories(directoryPath, "*", SearchOption.AllDirectories))
+                {
+                    allDirectories.Add(dir);
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+
+            Parallel.ForEach(allDirectories, new ParallelOptions { CancellationToken = token }, currentDir =>
+            {
+                try
+                {
+                    foreach (string file in Directory.EnumerateFiles(currentDir, searchPattern, SearchOption.TopDirectoryOnly))
+                    {
+                        token.ThrowIfCancellationRequested();
+
+                        this.Invoke((MethodInvoker)(() =>
+                        {
+                            listBoxResults.Items.Add(file);
+                        }));
+
+                        Interlocked.Increment(ref foundFilesCount);
+
+                        lock (logWriter)
+                        {
+                            logWriter.WriteLine(file);
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException) { }
+            });
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
+        }
+    }
+}
